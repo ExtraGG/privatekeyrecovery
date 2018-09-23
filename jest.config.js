ec6c4b9c54d7dbe52c7ce21df99aa63aa71ce147
GuusBaggen <baggenguus@gmail.com>
2018-09-23 13:34:27 +0200

    jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	moduleFileExtensions: [
+		'ts',
+		'js',
+		'json'
+	],
+	// transform: {
+	// 	'^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js'
+	// },
+	testMatch: [
+		'**/test/**/*.test.(ts|js)'
+	],
+	testEnvironment: 'node'
+};
--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
-const brokenKey = 'KwNryX9f7WSjXNPjnsaefBohLwG9GPK6Y7VhvJKSwsxL8oy'
 const base58 = require('bs58')
 const crypto = require('crypto')
-var randomString
-var tempKey = ''
 
-export default function recover (brokenKey, updateFrequency = 100000) {
-  // Implement question marks replacement for where it is unknown.
-  // cleanup code
-  // Add public key method
-  // Add quick method if least 5 characters or less are unknown at the enD,
-  // start with the amount of unknown characters as beginning iterator.
-  // end iterator by count of question marks (58^X)
-  // Publish it as an NPM package
-  for (let i = 0; i < 670000000; i++) {
-    if (!decode(tempKey)) {
-      randomString = encode(i)
-      tempKey = brokenKey + randomString
-      if (i % updateFrequency === 0) {
-        console.log(('Program is at: ' + (i / 670000000 * 100).toPrecision(3) + '%'))
-        console.log('It tried it with: ' + randomString)
-        console.log(i)
-        console.log(tempKey)
-      }
-      continue
-    } else {
-      console.log(tempKey)
-      return
-    }
-  }
-}
-
-function encode (enc) {
+function encode(enc) {
   const alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
   const base = alphabet.length
   if (typeof enc !== 'number') { throw Error('"encode" only accepts integers.') }
@@ -43,7 +14,7 @@ function encode (enc) {
   return encoded
 }
 
-function decode (string) {
+function decode(string) {
   const buffer = Buffer.from(base58.decode(string))
   let prefix = buffer.slice(0, 1)
   let data = buffer.slice(1, -4)
@@ -57,5 +28,28 @@ function decode (string) {
     return true
   }
 }
-
-recover(brokenKey)
+module.exports.recover = function (brokenKey, updateFrequency = 100000) {
+  var tempkey = ''
+  // Implement question marks replacement for where it is unknown.
+  // cleanup code
+  // Add public key method
+  // Add quick method if least 5 characters or less are unknown at the enD,
+  // start with the amount of unknown characters as beginning iterator.
+  // end iterator by count of question marks (58^X)
+  // Publish it as an NPM package
+  for (let i = 0; i < 670000000; i++) {
+    if (!decode(this.tempKey)) {
+      tempKey = brokenKey + encode(i)
+      if (i % updateFrequency === 0) {
+        console.log(('Program is at: ' + (i / 670000000 * 100).toPrecision(3) + '%'))
+        console.log('It tried it with: ' + encode(i))
+        console.log(i)
+        console.log(tempKey)
+      }
+      continue
+    } else {
+      console.log(tempKey)
+      return tempkey
+    }
+  }
+}
