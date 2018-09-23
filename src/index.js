b22553822610e3e202e982bb0098b3ba8fe51004
GuusBaggen <baggenguus@gmail.com>
2018-09-23 12:28:22 +0200

    formatting

--- src/index.js
+++ src/index.js
@@ -1,60 +1,61 @@
-const brokenKey = "KwNryX9f7WSjXNPjnsaefBohLwG9GPK6Y7VhvJKSwsxL8oy";
-// const base58 = require('bs58')
+const brokenKey = 'KwNryX9f7WSjXNPjnsaefBohLwG9GPK6Y7VhvJKSwsxL8oy'
+const base58 = require('bs58')
 const crypto = require('crypto')
-let randomString;
-let tempKey = "";
+var randomString
+var tempKey = ''
 
-recover(brokenKey);
-
-export default function recover(brokenKey, updateFrequency = 100000) {
-  // Implement question marks replacement for where it is unknown. 
+export default function recover (brokenKey, updateFrequency = 100000) {
+  // Implement question marks replacement for where it is unknown.
   // cleanup code
-  // Publish it as an NPM package
   // Add public key method
-  // Add quick method if last 5 characters or less are unknown
-  for (let i = 55000000; i < 670000000; i++) {
+  // Add quick method if least 5 characters or less are unknown at the enD,
+  // start with the amount of unknown characters as beginning iterator.
+  // end iterator by count of question marks (58^X)
+  // Publish it as an NPM package
+  for (let i = 0; i < 670000000; i++) {
     if (!decode(tempKey)) {
-      randomString = encode(i);
-      tempKey = brokenKey + randomString;
+      randomString = encode(i)
+      tempKey = brokenKey + randomString
       if (i % updateFrequency === 0) {
-        console.log(("Program is at: " + (i / 670000000 * 100).toPrecision(3) + "%"));
-        console.log("It tried it with: " + randomString);
-        console.log(i);
-        console.log(tempKey);
+        console.log(('Program is at: ' + (i / 670000000 * 100).toPrecision(3) + '%'))
+        console.log('It tried it with: ' + randomString)
+        console.log(i)
+        console.log(tempKey)
       }
-      continue;
+      continue
     } else {
-      console.log(tempKey);
-      return tempkey;
+      console.log(tempKey)
+      return
     }
   }
 }
 
-function encode(enc) {
-  const alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-  const base = alphabet.length;
-  if (typeof enc !== "number")
-    throw '"encode" only accepts integers.';
-  let encoded = "";
+function encode (enc) {
+  const alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+  const base = alphabet.length
+  if (typeof enc !== 'number') { throw Error('"encode" only accepts integers.') }
+  var encoded = ''
   while (enc) {
-    const remainder = enc % base;
-    enc = Math.floor(enc / base);
-    encoded = alphabet[remainder].toString() + encoded;
+    const remainder = enc % base
+    enc = Math.floor(enc / base)
+    encoded = alphabet[remainder].toString() + encoded
   }
-  return encoded;
+  return encoded
 }
 
-function decode(string) {
-  const buffer = new Buffer(base58.decode(string))
+function decode (string) {
+  const buffer = Buffer.from(base58.decode(string))
   let prefix = buffer.slice(0, 1)
   let data = buffer.slice(1, -4)
   let hash = Buffer.concat([prefix, data])
   hash = crypto.createHash('sha256').update(hash).digest()
   hash = crypto.createHash('sha256').update(hash).digest()
   if (!buffer.slice(-4).equals(hash.slice(0, 4))) {
-    return false;
+    return false
   } else {
-    console.log(string);
-    return true;
-  };
+    console.log(string)
+    return true
+  }
 }
+
+recover(brokenKey)
