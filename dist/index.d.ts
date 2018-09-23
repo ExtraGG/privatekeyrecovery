d19167b8ed81615735fbc776fdcbd51ff2107d05
GuusBaggen <baggenguus@gmail.com>
2018-09-23 20:50:57 +0200

    typings

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export default function recover(brokenKey: string, updateFrequency?: number, iteratorStart?: number): string;
---
+++ dist/utils.d.ts
@@ -0,0 +1,2 @@
+export declare function encode(enc: number): string;
+export declare function decode(input: string): boolean;
